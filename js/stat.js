9f716eadbded82af35de094d8ba690566bf31931
Anastasiya Vasina <nast791@mail.ru>
2017-08-14 00:14:50 +0300

    Доделала цвета

--- js/stat.js
+++ js/stat.js
@@ -1,28 +1,30 @@
+'use strict';
+
 window.renderStatistics = function (ctx, names, times) {
-  //тень
+  // тень
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
-  //облако
+  // облако
   ctx.fillStyle = 'white';
   ctx.fillRect(100, 10, 420, 270);
-  //текст
+  // текст
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура, вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-  //найдем самый большой результат для расчета высоты гистограмм
+  // найдем самый большой результат для расчета высоты гистограмм
   var max = -1;
 
-  for (var i = 0 ; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
     }
   }
 
-  //переменные
+  // переменные
   var histogramHeight = 150;
   var columnWidth = 40;
   var columnIndentX = 50;
@@ -30,22 +32,18 @@ window.renderStatistics = function (ctx, names, times) {
   var initialX = 120;
   var initialY = 100;
 
-  //гистограмма
-  for(var i = 0; i < times.length; i++) {
+  // гистограмма
+  for (var i = 0; i < times.length; i++) {
 
-    // for(var i = 0; i < names.length; i++) {
-    //   if (names[i] = 'Вы') {
-    //     ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    //   } else {
-    //     ctx.fillStyle = blue;
-    //   }
-    // }
+    // цвет столбиков
+    ctx.fillStyle = names[i] === 'Вы' ?
+      'rgba(255, 0, 0, 1)' :
+      'rgba(0, 0, 255,' + (Math.random() + 0.1).toFixed(1) + ')';
 
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    ctx.fillRect(initialX + columnIndentX * i, initialY + (max * histogramHeight/max - times[i] * histogramHeight/max), columnWidth, times[i] * histogramHeight/max);
+    ctx.fillRect(initialX + columnIndentX * i, initialY + (max * histogramHeight / max - times[i] * histogramHeight / max), columnWidth, times[i] * histogramHeight / max);
     ctx.fillStyle = '#000';
 
-    //текст
+    // текст
     ctx.textBaseline = 'top';
     ctx.fillText(names[i], initialX + columnIndentX * i, initialY + histogramHeight + textIndentY);
     ctx.textBaseline = 'bottom';
